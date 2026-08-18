103726e043b059c2a827247f1feda576765d392d
Aman001010 <amanahuja5300@gmail.com>
2026-08-19 01:39:24 +0530

    PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-
+#include <sstream>
+#include <cstdlib>
+#include <unistd.h>
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -31,18 +33,54 @@ int main() {
     std::cout << output << std::endl;
   }
   else if(firstword=="type"){
-        int len = input.length();
+    int len = input.length();
     std::string command = input.substr(5,len+1);
+
     if (command == "echo" | command == "type" | command == "exit"){
       std::cout<< command+" is a shell builtin"<<std::endl;
     }
-    else if (command == "grep"){
-      
-    }
+    
     else{
-      std::cout<< command+": not found"<<std::endl;
+      // the below is responsible of getting the entire path to search for the command and find out if it exists in the OS or not
+      int count=0; //for the found/not found of executable when searching
+      if(const char* full_path = std::getenv("PATH")){ // gives PATH to full_path
+
+        //stringstream object ss created, stringstream allows us to either convert string character type from say string to int OR  (the use here) to parse the string 
+        std::stringstream ss(full_path);
+
+        std::string directory;
+        
+
+        while(std::getline(ss,directory,':')){//getline is used for taking string input due to the delimeter being by default '\n' delimeter here we read ss till it reaches : and add that to directory
+
+          if(directory.empty()){ //this ensures that if the striing has something like ::usr/bin:... the empty path (:: part) doesn't come in the modified version since that would make the path //usr/bin which is obv invalid 
+            continue;
+          }
+          std::string filepath = directory+"/"+command;
+          
+          //now I need to take this filepath and search in it if executable exists
+
+          if(access(filepath.c_str(),X_OK)==0){
+            std::cout<< command+" is "+filepath<<"\n";
+            count=1;
+          }
+          
+
+        }
+        if(count==0){
+          
+            std::cout<<command+": not found"<<std::endl;
+          
+        }
+
+
+      }
+      else{
+        std::cout<< command+": not found" <<std::endl;
+      }
     }
   }
+  
   else{
   std::cout << input+": command not found"<<std::endl;
   }
